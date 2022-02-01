1dbecc1528f296bc838cb20eae73091a71b043c4
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-02-01 09:21:01 -0500

    added css back

--- ui/src/holochain-app.ts
+++ ui/src/holochain-app.ts
@@ -31,20 +31,31 @@ export class HolochainApp extends LitElement {
       </main>
 
       <p class="app-footer">
-        Rearanging elecrons.
-      </p>
+      🚽 Rearranging Electrons
+    </p>
     `;
   }
 
   static styles = css`
+    :host {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: #1a2b42;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: var(--lit-element-background-color);
+    }
     main {
       flex-grow: 1;
     }
-
     .app-footer {
       font-size: calc(12px + 0.5vmin);
       align-items: center;
-      align: bottom;
     }
   `;
 }
